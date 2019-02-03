32347fd22be4a9c6bae55dbd9df8c67503eb08b2
SamFerguson <fergusonsd@g.cofc.edu>
2019-02-03 09:34:59 -0500

    Added WOW factor, (the buttons are now drums)

--- app/src/main/java/howdyfellas/button_counter_ferguson/MainActivity.java
+++ app/src/main/java/howdyfellas/button_counter_ferguson/MainActivity.java
@@ -1,10 +1,12 @@
 package howdyfellas.button_counter_ferguson;
 
+import android.media.AudioAttributes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.media.SoundPool;
 
 /*
 Create an app that has three buttons, one for incrementing a counter, one for decrementing the counter,
@@ -20,38 +22,69 @@ public class MainActivity extends AppCompatActivity {
     private Button increment;
     private Button decrement;
     private Button reset;
-
+    private String hooey = "key";
+    private SoundPool sound;
+    private int kick, hat, snare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        increment = (Button) findViewById(R.id.increment);
-        decrement = (Button) findViewById(R.id.decrement);
-        reset = (Button) findViewById(R.id.reset);
-        count = (TextView) findViewById(R.id.count);
-        increment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                counter++;
-                count.setText(getString(R.string.total)+ ": " + counter);
+            super.onCreate(savedInstanceState);
+            if(savedInstanceState != null){
+                savedInstanceState.getInt(hooey);
             }
-        });
-        decrement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                counter--;
-                count.setText(getString(R.string.total)+": " + counter);
-            }
-        });
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                counter= 0;
-                count.setText(getString(R.string.total)+": " + counter);
-            }
-        });
+            setContentView(R.layout.activity_main);
 
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+            sound = new SoundPool.Builder()
+                    .setMaxStreams(3)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+            kick = sound.load(this, R.raw.kick, 1);
+            hat = sound.load(this, R.raw.hat, 1);
+            snare = sound.load(this, R.raw.snare, 1);
+
+            increment = (Button) findViewById(R.id.increment);
+            decrement = (Button) findViewById(R.id.decrement);
+            reset = (Button) findViewById(R.id.reset);
+            count = (TextView) findViewById(R.id.count);
+
+
+            increment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    counter++;
+                    sound.play(kick, 1,1,0,0,1);
+                    count.setText(getString(R.string.total)+ ": " + counter);
+                }
+            });
+            decrement.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    counter--;
+                    sound.play(hat, 1,1,0,0,1);
+                    count.setText(getString(R.string.total)+": " + counter);
+                }
+            });
+            reset.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sound.play(snare, 1, 1, 1, 0,1);
+                    counter= 0;
+                    count.setText(getString(R.string.total)+": " + counter);
+                }
+            });
+        }
+
+    /*
+    annoyingly enough whenever you tilt the screen it clears all of my stuff
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(hooey, counter);
     }
 
 }
